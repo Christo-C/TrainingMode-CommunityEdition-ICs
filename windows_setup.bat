f6cd063c84a04efe6d0623c7e408c6757ddb2ae4
Chris Cervania <christophercervania@gmail.com>
2025-08-09 01:37:52 -0600

    reverted makefile and adjusted setup bat file for D: drive

--- windows_setup.bat
+++ windows_setup.bat
@@ -4,19 +4,19 @@ setlocal
 REM set directory to location of this file. Needed when drag n dropping across directories.
 cd /d %~dp0
 
-if not exist "C:/devkitPro/devkitPPC" (
-    echo ERROR: devkitPro not found at "C:/devkitPro/devkitPPC"
+if not exist "D:/devkitPro/devkitPPC" (
+    echo ERROR: devkitPro not found at "D:/devkitPro/devkitPPC"
     echo Please install devkitPro with the GameCube package
     goto end
 ) else (
     echo found devkitPro
-    set "PATH=%PATH%;C:\devkitPro\devkitPPC\bin"
+    set "PATH=%PATH%;D:\devkitPro\devkitPPC\bin"
 )
 
-C:\devkitPro\msys2\msys2_shell.cmd -use-full-path -msys2 -here
+D:\devkitPro\msys2\msys2_shell.cmd -use-full-path -msys2 -here
 exit 1
 
 :end
 
 REM pause if not run from command line
-echo %CMDCMDLINE% | findstr /C:"/c">nul && pause
+echo %CMDCMDLINE% | findstr /D:"/c">nul && pause
